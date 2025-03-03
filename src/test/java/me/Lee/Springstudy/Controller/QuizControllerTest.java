a4ec1abdbf24bca433ed0e214f22bf6afb57d7e8
DG <kk506kk@naver.com>
2025-03-03 11:53:33 +0900

    spring boot test practice

---
+++ src/test/java/me/Lee/Springstudy/Controller/QuizControllerTest.java
@@ -0,0 +1,112 @@
+package me.Lee.Springstudy.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class QuizControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void mockMvcSetup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+    }
+
+    @DisplayName("quiz() : GET code 1")
+    @Test
+    public void getQuiz1() throws Exception {
+        // given
+        final String url = "/quiz";
+        // when
+        final ResultActions result = mockMvc.perform(get(url)
+                // 파라미터 받기
+                .param("code","1"));
+        // then
+        result
+                // 응답코드 201
+                .andExpect(status().isCreated())
+                // 응답 본문 Created!
+                .andExpect(content().string("Created!"));
+
+    }
+
+    @DisplayName("quiz2() : GET code 2")
+    public void getQuiz2() throws Exception {
+        // given
+        final String url = "/quiz";
+        // when
+        final ResultActions result = mockMvc.perform(get(url)
+                .param("code","2"));
+        // then
+        result
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("Bad Request!"));
+    }
+
+    @DisplayName("quiz() : POST code 1")
+    @Test
+    public void postQuiz1() throws Exception {
+        // given
+        final String url = "/quiz";
+
+        // when
+        final ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new Code(1)))
+        );
+
+        // then
+        result
+                .andExpect(status().isForbidden())
+                .andExpect(content().string("Forbidden!"));
+    }
+
+    @DisplayName("quiz() : POST code 13")
+    @Test
+    public void postQuiz13() throws Exception {
+        // given
+        final String url = "/quiz";
+
+        // when
+        final ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new Code(13)))
+        );
+
+        // then
+        result
+                .andExpect(status().isOk())
+                .andExpect(content().string("OK!"));
+
+
+    }
+
+
+
+
+}
