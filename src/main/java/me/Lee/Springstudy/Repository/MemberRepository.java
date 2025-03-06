7b898fd756aaf21033fac28165c97911b3176718
DG <kk506kk@naver.com>
2025-03-06 15:45:30 +0900

    spring boot ORM,JPA,Hibernate,springDataJPA

--- src/main/java/me/Lee/Springstudy/Repository/MemberRepository.java
+++ src/main/java/me/Lee/Springstudy/Repository/MemberRepository.java
@@ -4,6 +4,10 @@ import me.Lee.Springstudy.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // name 으로 값을 찾기
+    Optional<Member> findByName(String name);
 }
